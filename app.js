80240c4b50e3d0e4e2b6e5ff0c7f73dd6a5d23db
Jahanzaibv2 <iam@ranajahanzaib.com>
2019-06-19 09:39:23 +0500

    .

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ let allApps = appsRef.get()
   });
 
 // An api endpoint that returns a list of items (apps)
-app.get('/v0/get/item', (req,res) => {
+app.get('/v1/get/item', (req,res) => {
   res.json(items);
 });
 
